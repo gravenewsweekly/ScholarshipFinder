616a48e3c4ad1527e761681b4089b53a5f4c2325
GraveNews <87426252+gravenewsweekly@users.noreply.github.com>
2025-03-23 23:49:56 +0530

    Create firebase-config.js

---
+++ docs/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOTZYWamm_i__xlJVy-JCXKkIc5ZLhIlM",
+  authDomain: "scholarshipfinder-42d2a.firebaseapp.com",
+  databaseURL: "https://scholarshipfinder-42d2a-default-rtdb.firebaseio.com",
+  projectId: "scholarshipfinder-42d2a",
+  storageBucket: "scholarshipfinder-42d2a.appspot.com",
+  messagingSenderId: "375638023475",
+  appId: "1:375638023475:web:55fbf1e1aa9f1d88147246",
+  measurementId: "G-QZM1V40MNR"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
